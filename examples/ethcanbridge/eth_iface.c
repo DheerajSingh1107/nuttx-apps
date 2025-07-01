4cbaed70b60341d4d67811569023b3590db81e94
Dheeraj Singh <Dheeraj.bisht@addverb.com>
2025-07-01 17:50:12 +0530

    new(AR-27): run tcp/ip with stm32f207zg for eth-can bridge

--- examples/ethcanbridge/eth_iface.c
+++ examples/ethcanbridge/eth_iface.c
@@ -13,8 +13,10 @@ static int client_fd = -1;
 
 static void *eth_server_thread(void *arg)
 {
-    int server_fd;
+    int server_fd, client_fd;
     struct sockaddr_in addr = {0};
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
 
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     addr.sin_family = AF_INET;
@@ -24,17 +26,26 @@ static void *eth_server_thread(void *arg)
     listen(server_fd, 1);
 
     printf("[ETH] Listening on port %d...\n", PORT);
-    client_fd = accept(server_fd, NULL, NULL);
+    client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
 
     uint8_t buf[64];
     while (1)
     {
         ssize_t len = read(client_fd, buf, sizeof(buf));
+
         if (len > 0)
-            //bridge_forward_from_eth(buf, len);
-            printf("[ETH] data on port %d...\n", PORT);
+          {
+            //printf("[ETH] data on port %d len : %d...\n", PORT,len);
+            if (send(client_fd, buf, len, 0) != len)
+              {
+                printf("ERROR: send failed");
+                break;
+              }
+          }
     }
 
+    close(client_fd);
+    close(server_fd);
     return NULL;
 }
 
